069fb7b251019604424d293ff782dc177702ab39
hill07 <jroy01520@gmail.com>
2026-02-06 09:55:38 +0530

    xml

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -8,7 +8,8 @@ export async function getServerSideProps({ res }) {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.openipo.in";
 
   try {
-    const { data: ipos } = await iposAPI.getAll({ limit: 1000 });
+    const response = await iposAPI.getAll({ limit: 1000 });
+    const ipos = response.data?.ipos || [];
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
